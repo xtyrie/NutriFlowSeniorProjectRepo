8e1cba450aa4adc525068eb33b50ca97ef6e2ec6
Jaroan Tatum <jaroantatum11@outlook.com>
2024-01-06 11:21:46 -0600

    Initial commit

---
+++ NutriFlowSeniorProject/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace NutriFlowSeniorProject
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ NutriFlowSeniorProject/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace NutriFlowSeniorProject
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
