56345fb79c578b36a269723ea491d28b4064dcb3
Benkolion1 <143370355+Benkolion1@users.noreply.github.com>
2024-01-24 22:20:05 -0600

    Add files via upload
    Base files for project

---
+++ NutriFlow/NutriFlow/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace NutriFlow;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
